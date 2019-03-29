455bfe4665799c4549bcba13be1f883189e8c797
app <aniketengg.225@gmail.com>
2019-03-29 16:52:40 +0530

    linting

--- test/utils/solc.js
+++ test/utils/solc.js
@@ -1,28 +1,30 @@
+'use strict';
+
 const solc = require('solc');
 const fs = require('fs');
 
 module.exports.compile = async (contractPath, contractName, enableOptimization) => {
-    var input = {
-        language: 'Solidity',
-        sources: {
-                'sample.sol': {
-                    content : fs.readFileSync(contractPath, 'utf8')
-                }
-            },
-        settings: {
-            outputSelection: {
-                '*': {
-                    '*': [ '*' ]
-                }
-            }
-        }
-    };
+  const input = {
+    language: 'Solidity',
+    sources: {
+      'sample.sol': {
+        content : fs.readFileSync(contractPath, 'utf8'),
+      },
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': [ '*' ],
+        },
+      },
+    },
+  };
 
-    if(enableOptimization)
-        input.settings.optimizer = {enabled: true};
-    let compilationData = JSON.parse(solc.compile(JSON.stringify(input))).contracts['sample.sol'][contractName];
-    let result = {};
-    result.bytecode = compilationData.evm.bytecode.object;
-    result.abi = compilationData.abi;
-    return result;
-}
+  if(enableOptimization)
+    input.settings.optimizer = { enabled: true };
+  const compilationData = JSON.parse(solc.compile(JSON.stringify(input))).contracts['sample.sol'][contractName];
+  const result = {};
+  result.bytecode = compilationData.evm.bytecode.object;
+  result.abi = compilationData.abi;
+  return result;
+};
