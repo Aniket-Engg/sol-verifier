500bace39f743742e42419476f90abf9f10b80d7
app <aniketengg.225@gmail.com>
2019-04-03 18:03:06 +0530

    fixes #15

--- bin/sol-verifier.js
+++ bin/sol-verifier.js
@@ -4,49 +4,69 @@
 const clc       = require('cli-color');
 const program     = require('commander');
 const { verify } = require('../lib/verify');
+const { getNetwork } = require('../utils/network');
 
 program
   .version(require('../package.json').version, '-v, --version')
   .option('-k, --key <etherscan-api-key>', 'Add Etherscan API Key (required)')
   .option('-c, --contract <path-to-solidity-contract-file>', 'Add Contract File Path (required)')
   .option('-a, --address <contract-address>', 'Add Address of Deployed Contract (required)')
-  .option('-n, --network <network>', 'Add Ethereum Network on Which Contract is deployed (required)')
+  .option('-n, --network <network>', 'Add Ethereum Network on Which Contract is deployed (if applicable)')
   .option('-N, --contractName <contract-name>', 'Add Contract Name if Passed File Contains More Than One Contract (if applicable)') // eslint-disable-line max-len
   .option('-p, --constructParams [param1, param2,...]', 'Add Constructor Parameter Values Same as in Deployment (if applicable)') // eslint-disable-line max-len
   .option('-o, --optimize', 'Add This Flag to Optimize The Contract (optional)')
   .parse(process.argv);
 
-if(!program.key || !program.contract || !program.address || !program.network)
-{
-  console.log(clc.red('Error: Required parameter not passed'));
-  program.outputHelp();
-}
-else{
-  if(program.constructParams)
-    program.constructParams = program.constructParams.slice(1, (program.constructParams.length -1)).split(',');
+(async function formalize (){
+  let network;
 
-  const data = {
-    key             :   program.key,
-    path            :   program.contract,
-    contractAddress :   program.address,
-    network         :   program.network,
-    contractName    :   program.contractName,
-    cvalues         :   program.constructParams,
-    optimizationFlag:   program.optimize,
-  };
-  verify(data)
-    .then(function (res){
+  if(!program.key || !program.contract || !program.address)
+  {
+    console.log(clc.red('Error: Required Parameter Not Passed'));
+    program.outputHelp();
+  }
+  else{
+    if(program.constructParams)
+      program.constructParams = program.constructParams.slice(1, (program.constructParams.length -1)).split(',');
+
+    if(program.network)
+      network = program.network;
+    else {
+      const availableNets = await getNetwork(program.address);
+      if(availableNets.length == 1){
+        network = availableNets[0];
+        console.log(clc.green(`Contract Address Found on ${network} Network!!! Verifying ...`));
+      }
+      else{
+        console.log(clc.red(`Contract Address Found on ${availableNets} Networks. Please Provide --network Option.`));
+        program.outputHelp();
+        process.exit();
+      }
+    }
+
+    const data = {
+      key             :   program.key,
+      path            :   program.contract,
+      contractAddress :   program.address,
+      network         :   network,
+      contractName    :   program.contractName,
+      cvalues         :   program.constructParams,
+      optimizationFlag:   program.optimize,
+    };
+
+    try{
+      const res = await verify(data);
       if(res.status == '1'){
         console.log(clc.green('Contract has been successfully verified. Your GUID receipt : ' + res.result));
       }
       else if(res.status == '0'){
         console.log(clc.red('Error: ' + res.result));
       }
-    })
-    .catch(function (error){
-      console.log(clc.red('Error: ' + error.message));
-    });
-}
+    }catch(error){
+      throw error;
+    }
+  }
+})();
 
 
 
